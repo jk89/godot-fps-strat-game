74110dab9d54bbcae7ffd5cdbde0585741663d25
jonathan <jonathan.s.kelsey@gmail.com>
2019-03-08 17:56:31 +0000

    +

--- main.gd
+++ main.gd
@@ -5,18 +5,19 @@ extends Spatial
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-onready var viewportMain = $Control/ViewportContainerMap/ViewportMain
-onready var viewportMap = $Control/ViewportContainerMap/ViewportMap
+
 
 onready var cameraMain = $Control/ViewportContainerMain/ViewportMain/CameraMain
 onready var cameraMap = $Control/ViewportContainerMap/ViewportMap/CameraMap
 onready var world = $Control/ViewportContainerMain/ViewportMain/world
 onready var treeRoot = get_tree().get_root()
+onready var viewportMain = treeRoot.find_node("ViewportMain", true, false)
+onready var viewportMap = treeRoot.find_node("ViewportMap", true, false)
 onready var worldNode = self#treeRoot.find_node("world", true, false)
-
 onready var allSelectableUnits = []
 func _ready():
-	#viewportMap.world = viewportMain.world
+	viewportMap.world = viewportMain.world
+	print(viewportMap)
 	pass
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
